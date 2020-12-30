9f66467fcdede67c49114680832c4942f924336d
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-30 02:59:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,8 +12,10 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
-  end
+     students_array.each do |student_hash|
+       Student.new(student_hash)
+     end
+   end
 
   def add_student_attributes(attributes_hash)
 
